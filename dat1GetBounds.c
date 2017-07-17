dfdaf9cfb974d341ead9122af733a226bd6a28a4
David Berry <d.berry@jach.hawaii.edu>
2017-07-17 13:59:42 +0100

    dat1GetBounds: Prevent HDF error if input is not a slice

--- dat1GetBounds.c
+++ dat1GetBounds.c
@@ -133,8 +133,14 @@ dat1GetBounds( const HDSLoc * locator, hdsdim lower[DAT__MXDIM],
 
     /* If we are using datSlice then there should be one (and only one) hyperslab
        for the dataspace and we need to handle that. Should be same dimensionality
-       as above. Negative number indicates there were no hyperslabs. */
-    nblocks = H5Sget_select_hyper_nblocks( locator->dataspace_id );
+       as above. Negative number indicates there were no hyperslabs. We need to
+       check that there is a hyperslab first because H5Sget_select_hyper_nblocks 
+       will report an error if there is no hyperslab selection. */
+    if( H5Sget_select_type( locator->dataspace_id ) == H5S_SEL_HYPERSLABS ) {
+       nblocks = H5Sget_select_hyper_nblocks( locator->dataspace_id );
+    } else {
+       nblocks = 0;
+    }
 
     if (nblocks == 1) {
       herr_t h5err = 0;
